9da8911348124c39c6901f76b9f5c25259d46b0b
kwebster2 <kevin.r.webster@gmail.com>
2019-07-20 18:44:00 -0300

    Solution

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,34 @@
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each do |item|
+    value = consolidated[item.keys[0]] ||= item.values[0].clone
+    value[:count] = value[:count].to_i + 1
+  end
+  consolidated
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.keys.include?(item_name = coupon[:item]) && cart[item_name][:count] >= coupon[:num]
+      cart[coupon_item = "#{item_name} W/COUPON"] ||= cart[item_name].clone.update(count: 0, price: coupon[:cost] / coupon[:num])
+      cart[item_name][:count] -= coupon[:num]
+      cart[coupon_item][:count] += coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each {|item, value| value[:price] = (value[:price] * 0.8).round(2) if value[:clearance]}
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  apply_coupons(consolidated_cart, coupons)
+  apply_clearance(consolidated_cart)
+  total = 0
+  consolidated_cart.each do |item, value|
+    total += value[:price] * value[:count]
+  end
+  total >= 100 ? total * 0.9 : total
 end
